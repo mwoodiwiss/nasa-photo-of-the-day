d980b3861ee8a6a2c7d37114b6ded6d3d2dc3291
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2019-08-29 22:29:23 -0600

    reache-mvp

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
+
+const divStyle = {
+    margin: 'auto',
+    maxWidth: '90%',
+    borderLeft: '3px solid grey',
+    borderRight: '3px solid grey',
+    background: 'white'
+
+  };
+  const pStyle = {
+    margin: '20px',
+    fontSize: '15px',
+    textAlign: 'left'
+  };
+  const aStyle = {
+    fontSize: '16px',
+    textAlign: 'center',
+    textDecoration: 'none'
+  };
+  const h1Style = {
+    fontSize: '50px',
+    textAlign: 'center',
+    color: 'blue'
+  };
+  const h2Style = {
+    fontSize: '25px',
+    textAlign: 'center'
+  };
 
 function PhotoCard(props) {
     return (
-        <div class='photo-card'>
-            <img class='image' src='#' alt='NASA image of the day'></img>
-
-            <div class='text'>
-                <h2 class='title-text'>yo</h2>
-                <h3 class='text-content'>sup</h3>
-                <a href="#">ayyy</a>
+        <div className='photo-card' style={divStyle}>
+            <h1 className='title-text' style={h1Style}>A super awesome component!</h1>
+            <img className='image' src={props.url} alt='NASA image of the day'/>
+            <div className='text'>
+                <h2 className='title-text' style={h2Style}>{props.title}</h2>
+                <h3 className='text-content' style={pStyle}>{props.explanation}</h3>
+                <a href={props.url} style={aStyle}>View source</a>
             </div>
+            <footer> 
+                <p>Copyright:{props.copyright}</p>
+            </footer>
         </div>
     );
 }
